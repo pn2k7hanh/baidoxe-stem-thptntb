f22055a2a03bd1ae10b53eb57a76f403fd3b4fa6
algori <pn2k7hanh@gmail.com>
2023-12-09 09:45:31 +0700

    remove "com"

--- com/adafruit.py
+++
@@ -1,8 +0,0 @@
-from Adafruit_IO import Client, Feed
-
-from PyQt5.QtCore import *
-
-class Adafruit(QThread):
-	def __init__(self,parent=None):
-		QThread.__init__(self,parent)
-		
--- com/uic.bat
+++
@@ -1,2 +0,0 @@
-pyuic5 mainwindow.ui -o ui_mainwindow.py
-pyuic5 settingsdialog.ui -o ui_settingsdialog.py
